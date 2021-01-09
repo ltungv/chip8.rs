9ae380e51cf4dbc4b8d1bdc868fd3985655933e6
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-01-09 10:40:05 +0700

    flow control enum

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub const CHIP8_SCREEN_WIDTH: usize = 64;
 /// Screen height of chip-8
 pub const CHIP8_SCREEN_HEIGHT: usize = 32;
 /// Size of each pixel when render to the host machine
-pub const PIXEL_SIZE: i32 = 20;
+pub const PIXEL_SIZE: i32 = 16;
 
 /// This struct represents the CPU structure of CHIP-8 systems
 pub struct Chip8 {
@@ -82,6 +82,7 @@ impl EventHandler for Chip8 {
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         if self.can_draw {
+            self.can_draw = false;
             graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
             for y in 0..CHIP8_SCREEN_HEIGHT {
                 for x in 0..CHIP8_SCREEN_WIDTH {
@@ -224,6 +225,13 @@ impl EventHandler for Chip8 {
     }
 }
 
+enum Flow {
+    Halt,
+    Next,
+    Skip,
+    Jump(u16),
+}
+
 impl Chip8 {
     /// Set the state of the system to the intial state
     pub fn reset(&mut self) {
@@ -282,7 +290,13 @@ impl Chip8 {
         let opcode = (self.mem[pc] as u16) << 8 | self.mem[pc + 1] as u16;
 
         // Process the opcode
-        self.decode_exec(opcode);
+        let flow = self.decode_exec(opcode);
+        match flow {
+            Flow::Halt => {}
+            Flow::Next => self.pc += 2,
+            Flow::Skip => self.pc += 4,
+            Flow::Jump(addr) => self.pc = addr,
+        }
 
         // Update timers
         // The two timers count down to zero if they have been set to a
@@ -298,23 +312,23 @@ impl Chip8 {
         }
     }
 
-    fn decode_exec(&mut self, opcode: u16) {
-        let opcode_n0 = (opcode & 0xF000) >> 12;
-        let opcode_n1 = (opcode & 0x0F00) >> 8;
-        let opcode_n2 = (opcode & 0x00F0) >> 4;
-        let opcode_n3 = opcode & 0x000F;
-
-        let x = opcode_n1 as usize;
-        let y = opcode_n2 as usize;
-        let n = opcode & 0x000F;
+    fn decode_exec(&mut self, opcode: u16) -> Flow {
+        let nibbles = (
+            (opcode & 0xF000) >> 12,
+            (opcode & 0x0F00) >> 8,
+            (opcode & 0x00F0) >> 4,
+            (opcode & 0x000F),
+        );
+        let x = nibbles.1 as usize;
+        let y = nibbles.2 as usize;
+        let n = nibbles.3;
         let kk = (opcode & 0x00FF) as u8;
         let nnn = opcode & 0x0FFF;
 
-        match (opcode_n0, opcode_n1, opcode_n2, opcode_n3) {
+        match nibbles {
             // 00E0 - CLS
             // Clear the display.
             (0x0, 0x0, 0xE, 0x0) => {
-                // println!("00E0 - CLS");
                 self.gfx.iter_mut().for_each(|pixel| *pixel = false);
                 self.can_draw = true;
             }
@@ -323,7 +337,6 @@ impl Chip8 {
             // Return from a subroutine.
             // The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.
             (0x0, 0x0, 0xE, 0xE) => {
-                // println!("00EE - RET");
                 self.pc = self.stack[self.sp as usize];
                 self.sp -= 1;
             }
@@ -331,105 +344,83 @@ impl Chip8 {
             // 1NNN - JP addr
             // Jump to location nnn.
             // The interpreter sets the program counter to nnn.
-            (0x1, _, _, _) => {
-                // println!("1NNN - JP {}", nnn);
-                self.pc = nnn;
-                return;
-            }
+            (0x1, _, _, _) => return Flow::Jump(nnn),
 
             // 2NNN - CALL addr
             // Call subroutine at nnn.
             // The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
             (0x2, _, _, _) => {
-                // println!("2NNN - CALL {}", nnn);
                 self.sp += 1;
                 self.stack[self.sp as usize] = self.pc;
-                self.pc = nnn;
-                return;
+                return Flow::Jump(nnn);
             }
 
             // 3XKK - SE Vx, byte
             // Skip next instruction if Vx = kk.
             // The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
             (0x3, _, _, _) => {
-                // println!("3XKK - SE V{}, {}", x, kk);
-                self.pc += if self.v[x] == kk { 2 } else { 0 };
+                if self.v[x] == kk {
+                    return Flow::Skip;
+                }
             }
 
             // 4XKK - SNE Vx, byte
             // Skip next instruction if Vx != kk.
             // The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
             (0x4, _, _, _) => {
-                // println!("4XKK - SNE V{}, {}", x, kk);
-                self.pc += if self.v[x] != kk { 2 } else { 0 };
+                if self.v[x] != kk {
+                    return Flow::Skip;
+                }
             }
 
             // 5XY0 - SE Vx, Vy
             // Skip next instruction if Vx = Vy.
             // The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
             (0x5, _, _, 0x0) => {
-                // println!("5XY0 - SE V{}, V{}", x, y);
-                self.pc += if self.v[x] == self.v[y] { 2 } else { 0 };
+                if self.v[x] == self.v[y] {
+                    return Flow::Skip;
+                }
             }
 
             // 6XKK - LD Vx, byte
             // Set Vx = kk.
             // The interpreter puts the value kk into register Vx.
-            (0x6, _, _, _) => {
-                // println!("6XKK - LD V{}, {}", x, kk);
-                self.v[x] = kk;
-            }
+            (0x6, _, _, _) => self.v[x] = kk,
 
             // 7XKK - ADD Vx, byte
             // Set Vx = Vx + kk.
             // Adds the value kk to the value of register Vx, then stores the result in Vx.
-            (0x7, _, _, _) => {
-                // println!("7XKK - ADD V{}, {}", x, kk);
-                self.v[x] = self.v[x].wrapping_add(kk);
-            }
+            (0x7, _, _, _) => self.v[x] = self.v[x].wrapping_add(kk),
 
             // 8XY0 - LD Vx, Vy
             // Set Vx = Vy.
             // Stores the value of register Vy in register Vx.
-            (0x8, _, _, 0x0) => {
-                // println!("8XY0 - LD V{}, V{}", x, y);
-                self.v[x] = self.v[y];
-            }
+            (0x8, _, _, 0x0) => self.v[x] = self.v[y],
 
             // 8XY1 - OR Vx, Vy
             // Set Vx = Vx OR Vy.
             // Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the corrseponding bits
             // from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, it is 0.
-            (0x8, _, _, 0x1) => {
-                // println!("8XY1 - OR V{}, V{}", x, y);
-                self.v[x] |= self.v[y];
-            }
+            (0x8, _, _, 0x1) => self.v[x] |= self.v[y],
 
             // 8XY2 - AND Vx, Vy
             // Set Vx = Vx AND Vy.
             // Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the corrseponding bits
             // from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, it is 0.
-            (0x8, _, _, 0x2) => {
-                // println!("8XY2 - AND V{}, V{}", x, y);
-                self.v[x] &= self.v[y];
-            }
+            (0x8, _, _, 0x2) => self.v[x] &= self.v[y],
 
             // 8XY3 - XOR Vx, Vy
             // Set Vx = Vx XOR Vy.
             // Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. An exclusive OR compares
             // the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the result
             // is set to 1. Otherwise, it is 0.
-            (0x8, _, _, 0x3) => {
-                // println!("8XY3 - XOR V{}, V{}", x, y);
-                self.v[x] ^= self.v[y];
-            }
+            (0x8, _, _, 0x3) => self.v[x] ^= self.v[y],
 
             // 8XY4 - ADD Vx, Vy
             // Set Vx = Vx + Vy, set VF = carry.
             // The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0.
             // Only the lowest 8 bits of the result are kept, and stored in Vx.
             (0x8, _, _, 0x4) => {
-                // println!("8XY4 - ADD V{}, V{}", x, y);
                 let (res, overflow) = self.v[x].overflowing_add(self.v[y]);
                 self.v[0xF] = if overflow { 1 } else { 0 };
                 self.v[x] = res;
@@ -439,7 +430,6 @@ impl Chip8 {
             // Set Vx = Vx - Vy, set VF = NOT borrow.
             // If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
             (0x8, _, _, 0x5) => {
-                // println!("8XY5 - SUB V{}, V{}", x, y);
                 let (res, overflow) = self.v[x].overflowing_sub(self.v[y]);
                 self.v[0xF] = if overflow { 0 } else { 1 };
                 self.v[x] = res;
@@ -449,7 +439,6 @@ impl Chip8 {
             // Set Vx = Vx SHR 1.
             // If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
             (0x8, _, _, 0x6) => {
-                // println!("8XY6 - SHR V{}", x);
                 self.v[0xF] = self.v[x] & 0x01;
                 self.v[x] >>= 1;
             }
@@ -458,7 +447,6 @@ impl Chip8 {
             // Set Vx = Vy - Vx, set VF = NOT borrow.
             // If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
             (0x8, _, _, 0x7) => {
-                // println!("8XY7 - SUBN V{}, V{}", x, y);
                 let (res, overflow) = self.v[y].overflowing_sub(self.v[x]);
                 self.v[0xF] = if overflow { 0 } else { 1 };
                 self.v[x] = res;
@@ -468,7 +456,6 @@ impl Chip8 {
             // Set Vx = Vx SHL 1.
             // If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
             (0x8, _, _, 0xE) => {
-                // println!("8XYE - SHL V{}", x);
                 self.v[0xF] = (self.v[x] & 0x80) >> 7;
                 self.v[x] <<= 1;
             }
@@ -477,35 +464,29 @@ impl Chip8 {
             // Skip next instruction if Vx != Vy.
             // The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
             (0x9, _, _, 0x0) => {
-                // println!("9XY0 - SNE V{}, V{}", x, y);
-                self.pc += if self.v[x] != self.v[y] { 2 } else { 0 };
+                if self.v[x] != self.v[y] {
+                    return Flow::Skip;
+                }
             }
 
             // ANNN - ld i, addr
             // set i = nnn.
             // the value of register i is set to nnn.
-            (0xA, _, _, _) => {
-                // println!("ANNN - LD I, {}", nnn);
-                self.i = nnn;
-            }
+            (0xA, _, _, _) => self.i = nnn,
 
             // BNNN - JP V0, addr
             // Jump to location nnn + V0.
             // The program counter is set to nnn plus the value of V0.
             (0xB, _, _, _) => {
-                // println!("BNNN - JP V0, {}", nnn);
                 self.pc = self.v[0] as u16 + nnn;
-                return;
+                return Flow::Jump(nnn);
             }
 
             // CXKK - RND Vx, byte
             // Set Vx = random byte AND kk.
             // The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx.
             // See instruction 8xy2 for more information on AND.
-            (0xC, _, _, _) => {
-                // println!("CXKK - RND V{}, {}", x, kk);
-                self.v[x] = random::<u8>() & kk;
-            }
+            (0xC, _, _, _) => self.v[x] = random::<u8>() & kk,
 
             // Dxyn - DRW Vx, Vy, nibble
             // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
@@ -515,8 +496,6 @@ impl Chip8 {
             // it wraps around to the opposite side of the screen. See instruction 8xy3 for more information on XOR, and section 2.4,
             // Display, for more information on the Chip-8 screen and sprites.
             (0xD, _, _, _) => {
-                // println!("Dxyn - DRW V{}, V{}, {}", x, y, n);
-
                 self.v[0xF] = 0;
                 for (y_offset, sprite) in self.mem[self.i as usize..(self.i + n) as usize]
                     .iter()
@@ -533,7 +512,6 @@ impl Chip8 {
                         }
                     }
                 }
-
                 self.can_draw = true;
             }
 
@@ -541,79 +519,67 @@ impl Chip8 {
             // Skip next instruction if key with the value of Vx is pressed.
             // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
             (0xE, _, 0x9, 0xE) => {
-                // println!("Ex9E - SKP V{}", x);
-                self.pc += if self.key[self.v[x] as usize] { 2 } else { 0 };
+                if self.key[self.v[x] as usize] {
+                    return Flow::Skip;
+                }
             }
 
             // ExA1 - SKNP Vx
             // Skip next instruction if key with the value of Vx is not pressed.
             // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
             (0xE, _, 0xA, 0x1) => {
-                // println!("ExA1 - SKNP V{}", x);
-                self.pc += if self.key[self.v[x] as usize] { 0 } else { 2 };
+                if !self.key[self.v[x] as usize] {
+                    return Flow::Skip;
+                }
             }
 
             // Fx07 - LD Vx, DT
             // Set Vx = delay timer value.
             // The value of DT is placed into Vx.
-            (0xF, _, 0x0, 0x7) => {
-                // println!("Fx07 - LD V{}, DT", x);
-                self.v[x] = self.dt;
-            }
+            (0xF, _, 0x0, 0x7) => self.v[x] = self.dt,
 
             // Fx0A - LD Vx, K
             // Wait for a key press, store the value of the key in Vx.
             // All execution stops until a key is pressed, then the value of that key is stored in Vx.
             (0xF, _, 0x0, 0xA) => {
-                // println!("Fx0A - LD V{}, K", x);
-                self.pc -= 2; // reset program counter to previous position
+                let mut pressed = false;
                 for (key_idx, key_pressed) in self.key.iter().enumerate() {
                     if *key_pressed {
                         self.v[x] = key_idx as u8;
-                        self.pc += 2; // revert program counter value
+                        pressed = true;
                     }
+                }
+                if !pressed {
+                    return Flow::Halt;
                 }
             }
 
             // Fx15 - LD DT, Vx
             // Set delay timer = Vx.
             // DT is set equal to the value of Vx.
-            (0xF, _, 0x1, 0x5) => {
-                // println!("Fx15 - LD DT, V{}", x);
-                self.dt = self.v[x];
-            }
+            (0xF, _, 0x1, 0x5) => self.dt = self.v[x],
 
             // Fx18 - LD ST, Vx
             // Set sound timer = Vx.
             // ST is set equal to the value of Vx.
-            (0xF, _, 0x1, 0x8) => {
-                // println!("Fx18 - LD ST, V{}", x);
-                self.st = self.v[x];
-            }
+            (0xF, _, 0x1, 0x8) => self.st = self.v[x],
 
             // Fx1E - ADD I, Vx
             // Set I = I + Vx.
             // The values of I and Vx are added, and the results are stored in I.
-            (0xF, _, 0x1, 0xE) => {
-                // println!("Fx1E - ADD I, V{}", x);
-                self.i = self.i.wrapping_add(self.v[x] as u16);
-            }
+            (0xF, _, 0x1, 0xE) => self.i = self.i.wrapping_add(self.v[x] as u16),
 
             // Fx29 - LD F, Vx
             // Set I = location of sprite for digit Vx.
             // The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. See section 2.4, Display,
             // for more information on the Chip-8 hexadecimal font.
-            (0xF, _, 0x2, 0x9) => {
-                // println!("Fx29 - LD F, V{}", x);
-                self.i = self.v[x] as u16 * 5 // each hexadecimal sprite is 5-byte long
-            }
+            (0xF, _, 0x2, 0x9) => self.i = self.v[x] as u16 * 5,
 
             // Fx33 - LD B, Vx
             // Store BCD representation of Vx in memory locations I, I+1, and I+2.
             // The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit
             // at location I+1, and the ones digit at location I+2.
             (0xF, _, 0x3, 0x3) => {
-                // println!("Fx33 - LD B, V{}", x);
                 self.mem[self.i as usize] = self.v[x] / 100;
                 self.mem[self.i as usize + 1] = (self.v[x] / 10) % 10;
                 self.mem[self.i as usize + 2] = (self.v[x] % 100) % 10;
@@ -623,7 +589,6 @@ impl Chip8 {
             // Store registers V0 through Vx in memory starting at location I.
             // The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
             (0xF, _, 0x5, 0x5) => {
-                // println!("Fx55 - LD [I], V{}", x);
                 self.mem[self.i as usize..=self.i as usize + x].copy_from_slice(&self.v[0..=x]);
                 self.i += x as u16 + 1;
             }
@@ -632,7 +597,6 @@ impl Chip8 {
             // Read registers V0 through Vx from memory starting at location I.
             // The interpreter reads values from memory starting at location I into registers V0 through Vx.
             (0xF, _, 0x6, 0x5) => {
-                // println!("Fx65 - LD V{}, [I]", x);
                 self.v[0..=x].copy_from_slice(&self.mem[self.i as usize..=self.i as usize + x]);
                 self.i += x as u16 + 1;
             }
@@ -640,6 +604,7 @@ impl Chip8 {
             // Unsupported opcode
             (_, _, _, _) => panic!("Opcode is not supported {:#04X}", opcode),
         }
-        self.pc += 2;
+
+        Flow::Next
     }
 }
